37f87f49981c4fb4f90948f9c2ba8a547d77da8f
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2017-07-14 09:48:13 -0400

    full sync with calendar

--- google.py
+++ google.py
@@ -3,9 +3,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 import json
 import httplib2
 from apiclient import discovery
+import datetime
+import dateutil.parser as dateparse
+import moment
 
 SERVICE_FILE='Reddit-5c99b0f4e099.json'
 CALENDAR_SCOPE = 'https://www.googleapis.com/auth/calendar'
+
+_calendarService = None
 
 def sheets(sheetId):
   gc = pygsheets.authorize(service_file=SERVICE_FILE, no_cache=True)
@@ -14,10 +19,64 @@ def sheets(sheetId):
 
   return sheet
 
-def calendar(calId):
+def calendar():
+  global _calendarService
+
+  if _calendarService is not None:
+    return _calendarService
 
   credentials = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_FILE, CALENDAR_SCOPE)
   http = credentials.authorize(httplib2.Http())
   service = discovery.build('calendar', 'v3', http=http)
+  _calendarService = service
 
   return service
+
+
+def getAllCalendarEvents(calId):
+  calsvc = calendar()
+
+  allEvents = []
+  pageToken = None
+  start = moment.now().subtract(days=1).datetime.isoformat() + 'Z'
+
+  while True:
+    events = calsvc.events().list(calendarId=calId, pageToken=pageToken, timeMin=start).execute()
+    for event in events['items']:
+      allEvents.append(event)
+    pageToken = events.get('nextPageToken')
+    if not pageToken:
+      break
+
+  fullDayEvents = []
+  for event in allEvents:
+    start = event['start']
+    if 'date' in start:
+      event['date'] = moment.date(start['date'])
+      fullDayEvents.append(event)
+    else:
+      # event['dateTime'] = dateparse.parse(start['dateTime'], ignoretz=True)
+      # do nothing with these for now
+      pass
+
+  fullDayEvents = sorted(fullDayEvents, key=lambda x: x['date'])
+
+  return fullDayEvents
+
+def createEvent(calId, event):
+  calsvc = calendar()
+
+  try: 
+    event = calsvc.events().insert(calendarId=calId, body=event).execute()
+    print('Created {0}'.format(event.get('htmlLink')))
+  except Exception as e:
+    print(e)
+
+def patchEvent(calId, eventId, event):
+  calsvc = calendar()
+
+  try:
+    event = calsvc.events().patch(calendarId=calId, eventId=eventId, body=event).execute()
+    print('Updated {0}'.format(event.get('htmlLink')))
+  except Exception as e:
+    print(e)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import google
+import datetime
+import re
+import moment
 
 SHEET_ID='1UaITm4_Con1iuegeSxaT-b8w_cQOK1BwV0ywwGLj_qo'
 CAL_ID='bug8mt2ki8cu2ms714euief6q4@group.calendar.google.com'
@@ -9,14 +12,28 @@ def main():
   doc = google.sheets(SHEET_ID)
 
   races = {}
+  raceArr = []
 
+  print('Processing spreadsheet....')
   for sheet in doc.worksheets():
     process_sheet(sheet, races)
 
-  calSvc = google.calendar()
+  for key in races.keys():
+    race = races[key]
+    if race['date'] is None or race['date'].date is None:
+      print("Warn: '{0}' does not have a date ({1})".format(race['name'], race['rawdate']))
+    elif race['date'] < moment.now():
+      pass
+    else:
+      raceArr.append(race)
 
+  raceArr = sorted(raceArr, key=lambda x: x['date'])
+
+  print('Downloading calendar...')
+  events = google.getAllCalendarEvents(CAL_ID)
   
-
+  print('Synching...')
+  sync(raceArr, events)
 
 
 def process_sheet(sheet, races):
@@ -28,21 +45,101 @@ def process_sheet(sheet, races):
 
 
 def process_row(row, races):
-  racename = row[1]
+  racename = safe(row[1])
+  if racename == '':
+    return
+
   if racename.lower() not in races:
     races[racename.lower()] = {}
     race = races[racename.lower()]
 
     race['users'] = []
-    race['name'] = row[1]
-    race['distance'] = row[3]
-    race['date'] = row[4]
-    race['city'] = row[5]
-    race['state'] = row[6]
-    race['start'] = row[7]
+    race['name'] = safe(row[1])
+    race['distance'] = safe(row[3])
+    race['city'] = safe(row[5])
+    race['state'] = safe(row[6])
+    race['start'] = safe(row[7])
+    race['date'] = None
+    race['rawdate'] = safe(row[4])
+
+    if (race['city'] != '' and race['state'] != ''):
+      race['location'] = '{0}, {1}'.format(race['city'], race['state'])
+    elif race['city'] != '':
+      race['location'] = race['city']
+    else:
+      race['location'] = race['state']
+
+    date = safe(row[4])
+    if date.strip() != '': 
+      try:
+        d = moment.date(date.strip())
+        race['date'] = d 
+      except:
+        pass
+
+
 
   race = races[racename.lower()]
-  race['users'].append(row[0])
+  race['users'].append(safe(row[0]))
+
+
+
+def sync(races, events):
+  eventDict = {}
+  for event in events:
+    eventDict[eventkey(event['date'], safe(event['summary']))] = event 
+
+  for race in races:
+    rkey = eventkey(race['date'], race['name'])
+
+    
+
+    if rkey in eventDict:
+      event = raceToEvent(race, eventDict[rkey]['id'])
+      if diff(event, eventDict[rkey]):
+        print('Patching {0}'.format(rkey))
+        google.patchEvent(CAL_ID, event['id'], event)
+    else:
+      event = raceToEvent(race)
+      print('Creating {0}'.format(rkey))
+      google.createEvent(CAL_ID, event)
+
+
+def raceToEvent(race, event_id=None):
+  event = {
+    'summary': race['name'],
+    'location': race['location'],
+    'start': {
+      'date': race['date'].format('YYYY-MM-DD')
+    },
+    'end': {
+      'date': race['date'].add(days=1).format('YYYY-MM-DD')
+    },
+    'description': '\n'.join(map(lambda u: '/u/'+u, race['users']))
+  }
+  if event_id is not None:
+    event['id'] = event_id
+
+  return event 
+
+def diff(event, gEvent):
+  if event['location'] != safe(gEvent['location']):
+    return True
+  if event['start']['date'] != gEvent['start']['date']:
+    return True
+  if event['description'] != safe(gEvent['description']):
+    return True 
+
+  return False
+
+
+
+def safe(st):
+  return st.encode('utf-8').strip()
+def short(dt):
+  return dt.format('YYYY-MM-DD')
+def eventkey(date, name):
+  return '{0}--{1}'.format(date.format('YYYY-MM-DD'), name.lower())
 
 
 
